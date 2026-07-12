1becd763241906094a9aa68b8d2504a1b7fc7956
Monsterredzockt <erzgehalt@gmail.com>
2026-07-12 23:51:41 +0200

    d

--- pipeOrgan.lua
+++ pipeOrgan.lua
@@ -1,2 +1,14 @@
-print("hello world")
-print("du bist ein ganz toller mensch :)")
+print("Pipe Organ Song 1")
+
+local bridge = peripheral.find("redstone_link_bridge")
+
+if not bridge then
+    print("Fehler: Keine Redstone Link Bridge gefunden!")
+    return
+end
+
+bridge.sendLinkSignal("minecraft:diamond", "minecraft:redstone", 15)
+
+sleep(3)
+
+bridge.sendLinkSignal("minecraft:diamond", "minecraft:redstone", 0)
